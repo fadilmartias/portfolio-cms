37ccb3364d090a8eb0449f49267dd7a0d5fe3afe
fadilmartias <fadilmartias26@gmail.com>
2023-06-13 08:13:51 +0700

    init commit, setup db, setup laravel ui

---
+++ app/Models/Project.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'link',
+        'img_path',
+        'tech',
+        'type',
+        'password',
+    ];
+
+    protected $casts = [
+        'tech' => 'array',
+    ];
+}
---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Project::create([
+            'title' => 'PTIPD UIN Suska Riau Website',
+            'description' => 'Company profile website for Pusat Teknologi Informasi dan Pangkalan Data UIN Suska Riau',
+            'link' => 'https://ptipd.uin-suska.ac.id/',
+            'img_path' => '/img/portfolio/ptipd.png',
+            'tech' => ['wordpress'],
+            'type' => 'Freelance Project',
+            'password' => bcrypt('cp')
+        ]);
+    }
+}
