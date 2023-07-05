d4ec791b5e1ac25be42ab9edabb540328380fc7f
fadilmartias <fadilmartias26@gmail.com>
2023-07-05 18:35:53 +0700

    add middleware auth to api routes

--- routes/api.php
+++ routes/api.php
@@ -15,4 +15,4 @@ use App\Http\Controllers\Api\ProjectController;
 |
 */
 
-Route::get('/projects', [ProjectController::class, 'index']);
+Route::get('/projects', [ProjectController::class, 'index'])->middleware('auth');
